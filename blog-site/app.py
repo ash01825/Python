4cbf43c9ebaacc52ac3574c73bc7a64de313b947
ash01825 <niteshtulshyan998@gmail.com>
2024-10-31 02:39:10 +0530

    commit

--- blog-site/app.py
+++ blog-site/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from posts import Posts
+from flask import Flask, render_template,request
+from posts import Posts,send_mail
 import requests
 posts = requests.get("https://api.npoint.io/759afb724056c22504b7").json()
 # post=[]
@@ -18,10 +18,15 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.get('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', msg_sent = False)
 
+@app.post('/contact')
+def recieve_data():
+    data = request.form
+    send_mail(name=data["name"], sender_mail = data["email"],number =  data["phone"], message=data["message"])
+    return render_template('contact.html', msg_sent= True)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- blog-site/posts.py
+++ blog-site/posts.py
@@ -1,3 +1,10 @@
+from dotenv import load_dotenv
+import os
+import smtplib
+
+load_dotenv()
+
+
 class Posts:
     def __init__(self,id,title,subtitle,body,author,date):
         self.id = id
@@ -6,4 +13,15 @@ class Posts:
         self.body = body
         self.author = author
         self.date=date
-    
+class send_mail():
+    def __init__(self,message,sender_mail,name,number):
+        load_dotenv()
+        self.email = os.getenv('EMAIL')
+        self.passwo = os.getenv('APP_PASSWORD')
+        connection = smtplib.SMTP("smtp.gmail.com", port=587)
+        connection.starttls()
+        connection.login(user=self.email, password=self.passwo)
+        connection.sendmail(from_addr=self.email,
+                        to_addrs=self.email,
+                        msg=f"Subject:New Message\n\nName: {name}\nEmail: {sender_mail}\nPhone: {number}\nMessage:{message}")
+        connection.close()
